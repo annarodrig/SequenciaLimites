027740735584b686c303e36a3773a812d5f7e099
Anna Beatriz <annab_oliver@hotmail.com>
2022-05-27 20:37:56 -0300

    Inicia exercicio

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int numeroUm, numeroDois;
+
+Console.Write("Início:");
+numeroUm = Convert.ToInt32 (Console.ReadLine()!);
+
+
+Console.Write("Fim:");
+numeroDois = Convert.ToInt32 (Console.ReadLine()!);
+
+string mensagem = "";
+
+try
+{
+    if (numeroDois < numeroUm)
+    {
+        throw new Exception ("O segundo número não pode ser menor que o primeiro");
+    }
+
+    while (numeroUm <= numeroDois)
+    {
+        mensagem += $"{numeroUm++}";
+    }
+    Console.WriteLine($"{mensagem}");
+}
+    catch (Exception e)
+{
+    Console.WriteLine($"{mensagem}");
+}
