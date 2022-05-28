cb03a8089299f07dc1bdc4827ef466d4e7911011
Anna Beatriz <annab_oliver@hotmail.com>
2022-05-28 17:25:23 -0300

    Finaliza o exercicio

--- Program.cs
+++ Program.cs
@@ -1,28 +1,51 @@
-﻿int numeroUm, numeroDois;
-
-Console.Write("Início:");
-numeroUm = Convert.ToInt32 (Console.ReadLine()!);
+﻿int inicio, fim;
 
 
-Console.Write("Fim:");
-numeroDois = Convert.ToInt32 (Console.ReadLine()!);
 
 string mensagem = "";
 
 try
 {
-    if (numeroDois < numeroUm)
+    Console.Write("Início:");
+    inicio = Convert.ToInt32 (Console.ReadLine()!);
+
+
+    Console.Write("Fim:");
+    fim = Convert.ToInt32 (Console.ReadLine()!);
+
+    if (fim < inicio)
     {
         throw new Exception ("O segundo número não pode ser menor que o primeiro");
     }
 
-    while (numeroUm <= numeroDois)
+    while (inicio <= fim)
     {
-        mensagem += $"{numeroUm++}";
+       
+        mensagem += $"{inicio++} ";
+
     }
+
+    //Exibe mensagem de sucesso
+    Console.BackgroundColor = ConsoleColor.Green;
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine($"{mensagem}");
+
+}
+  catch (FormatException e)
+{
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("É necessário digitar um número válido.");
+    Console.WriteLine(e.Message);
 }
     catch (Exception e)
 {
-    Console.WriteLine($"{mensagem}");
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine(e.Message);
+}
+finally
+{
+    Console.ResetColor();
+    Console.WriteLine("Obrigado por utilizar nosso programa!");
 }
